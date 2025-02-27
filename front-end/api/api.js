341eaee08515314548ca5206875ea8298df71262
ViniciusMateos <vinimateos.s@gmail.com>
2025-02-27 02:03:32 -0300

    Ajustes para para deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,12 +1,12 @@
 // Axios
-
+import "dotenv/config";
 import Axios from "axios";
 
-const URL = "http://localhost:3001";
+const { NODE_ENV } = process.env;
+const URL = NODE_ENV === 'development' ? "http://localhost:3001/api" : '/api';
 
 const responseArtist = await Axios.get(`${URL}/artists`);
 const responseSong = await Axios.get(`${URL}/songs`);
 
 export const artistArray = responseArtist.data;
 export const songsArray = responseSong.data;
-
