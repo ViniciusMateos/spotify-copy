b9b3a47f1f17dcc759ecd0c10b0afbbd9b2e092f
ViniciusMateos <vinimateos.s@gmail.com>
2025-02-27 02:30:32 -0300

    Ajuste de process

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,7 @@
 // Axios
-import "dotenv/config";
 import Axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === 'development' ? "http://localhost:3001/api" : '/api';
+const URL = "https://spotify-smz0.onrender.com/api";
 
 const responseArtist = await Axios.get(`${URL}/artists`);
 const responseSong = await Axios.get(`${URL}/songs`);
